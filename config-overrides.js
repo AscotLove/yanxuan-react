38f1ed8a12b4e8b0773db7bf0b814e912db5b433
AscotLove <jxae093663@163.com>
2019-10-08 19:32:37 +0800

    '初始化yanxuan项目'

---
+++ config-overrides.js
@@ -0,0 +1,24 @@
+const { override, fixBabelImports, addLessLoader, addDecoratorsLegacy,addWebpackAlias } = require('customize-cra');
+const { resolve } = require('path');
+
+module.exports = override(
+  // 动态加载antd文件
+  fixBabelImports('import', {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: true,
+  }),
+  // css样式设置
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { '@primary-color': '#1DA57A' },
+  }),
+  // 添加装饰器
+  addDecoratorsLegacy(),
+  // 配别名
+  addWebpackAlias({
+    '@pages': resolve(__dirname, 'src/pages'),
+    '@components': resolve(__dirname, 'src/components'),
+    '@api': resolve(__dirname, 'src/api'),
+  })
+);
---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class Header extends Component {
+
+  render() {
+    return <header>
+      <div className="m-defaultBanner">
+        <div className="brand">
+          <div className="brand-logo">严选</div>
+          <p>前往APP领1000元新用户现金礼包</p>
+          <p>网易自营 品质生活</p>
+        </div>
+        <a className="w-button" href="/">打开APP</a>
+      </div>
+      <div className="m-indexHd">
+        <div className="line">
+          <h1>网易严选</h1>
+          <input type="text" />
+            <div className="loginBtn">登录</div>
+        </div>
+      </div>
+    </header>
+  }
+}
+
+
+
+export default Header;
